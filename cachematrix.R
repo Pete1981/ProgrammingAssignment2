7ce023923faa61cdf5b6977416eac702f10833c9
peter.smith <peter.smith@spdspark.com>
2016-04-25 21:10:21 +0100

    adding assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  mcm <- NULL
+  set <- function(y) {
+    x <<- y
+    mcm <<- NULL
+  }
+  get <- function() x
+  setmcm <- function(solve) mcm <<- solve
+  getmcm <- function() mcm
+  list(set = set, get = get,
+       setmcm = setmcm,
+       getmcm = getmcm)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mcm <- x$getmcm()
+  if(!is.null(mcm)) {
+    message("getting cached data")
+    return(mcm)
+  }
+  data <- x$get()
+  mcm <- solve(data, ...)
+  x$setmcm(mcm)
+  mcm
 }
